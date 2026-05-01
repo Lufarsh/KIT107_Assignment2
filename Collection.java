968d6d47ec7164d2a6516e046e0f2e08199aeba4
Lufarsh <mkalilur@utas.edu.au>
2026-05-01 10:21:20 +1000

    implemented collection most search

--- Collection.java
+++ Collection.java
@@ -106,11 +106,58 @@ public class Collection implements CollectionInterface
         current = current.getNext();
     }
 }
-    public String most(char x)
+public String most(char x)
+{
+    if (isEmpty())
     {
         return "No data!";
     }
 
+    Node current = firstTeam;
+    Player best = null;
+
+    while (current != null)
+    {
+        Cluster c = (Cluster) current.getData();
+        Player p = c.most(x);
+
+        if (p != null)
+        {
+            if (best == null)
+            {
+                best = p;
+            }
+            else
+            {
+                int val1 = 0;
+                int val2 = 0;
+
+                switch (x)
+                {
+                    case 'g': val1 = p.getGoals(); val2 = best.getGoals(); break;
+                    case 'd': val1 = p.getDisposals(); val2 = best.getDisposals(); break;
+                    case 'c': val1 = p.getClangers(); val2 = best.getClangers(); break;
+                    case 'a': val1 = p.getFreesAgainst(); val2 = best.getFreesAgainst(); break;
+                    case 'm': val1 = p.getGames(); val2 = best.getGames(); break;
+                }
+
+                if (val1 >= val2)
+                {
+                    best = p;
+                }
+            }
+        }
+
+        current = current.getNext();
+    }
+
+    if (best == null)
+    {
+        return "No data!";
+    }
+
+    return best.toString();
+}
     public void summarise(String t)
     {
         if (isEmpty())
